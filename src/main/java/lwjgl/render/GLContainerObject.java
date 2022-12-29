47ac7be51a2dcd97236dde2a5aea299522af9d95
Lunkle <bobbob.donny@gmail.com>
2022-12-29 08:56:13 -0500

    Add Documentation

--- src/main/java/lwjgl/render/GLContainerObject.java
+++ src/main/java/lwjgl/render/GLContainerObject.java
@@ -1,5 +1,9 @@
 package lwjgl.render;
 
+/**
+ * An OpenGL container object contains other OpenGL regular objects. These types of objects are not able to be shared
+ * between OpenGL contexts.
+ */
 public abstract class GLContainerObject extends GLObject {
 
 }
--- src/main/java/lwjgl/render/GLRegularObject.java
+++ src/main/java/lwjgl/render/GLRegularObject.java
@@ -1,5 +1,8 @@
 package lwjgl.render;
 
+/**
+ * An OpenGL regular object is an object that can be shared between OpenGL contexts.
+ */
 public abstract class GLRegularObject extends GLObject {
 
 }
