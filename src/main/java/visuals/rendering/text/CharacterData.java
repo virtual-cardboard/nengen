3192c98f87aa8fee8e9502bd98482d5b0cc2d54b
Jay <jay.jrjren@gmail.com>
2023-06-26 16:24:45 -0400

    Add documentation for CharacterData

--- src/main/java/visuals/rendering/text/CharacterData.java
+++ src/main/java/visuals/rendering/text/CharacterData.java
@@ -1,14 +1,45 @@
 package visuals.rendering.text;
 
+/**
+ * Information about a character in a bitmap font. The fields are all in pixels, except for {@link CharacterData#page}.
+ *
+ * @author Jay
+ */
 public final class CharacterData {
 
+	/**
+	 * The x-coordinate of the character's location on the texture atlas.
+	 */
 	private final int x;
+	/**
+	 * The y-coordinate of the character's location on the texture atlas.
+	 */
 	private final int y;
+	/**
+	 * The width of the character on the texture atlas.
+	 */
 	private final int width;
+	/**
+	 * The width of the character on the texture atlas.
+	 */
 	private final int height;
+	/**
+	 * The horizontal offset of the character when it is displayed. This is usually a small number.
+	 */
 	private final int xOffset;
+	/**
+	 * The vertical offset of the character when it is displayed.
+	 * For example, the letter 'g' has a larger yOffset than 'l' because it is offset downwards.
+	 */
 	private final int yOffset;
+	/**
+	 * How far the cursor should advance horizontally after printing this character.
+	 * This is usually approximately equal to the width.
+	 */
 	private final int xAdvance;
+	/**
+	 * The index of the bitmap image that this character is on. This is usually 0.
+	 */
 	private final int page;
 
 	public CharacterData(int x, int y, int width, int height, int xOffset, int yOffset, int xAdvance, int page) {
