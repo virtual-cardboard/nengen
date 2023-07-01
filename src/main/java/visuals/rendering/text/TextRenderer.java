7917d77941ce93367f8015ef5a98966a66ccc8bb
Lunkle <d.ren.donny@gmail.com>
2023-07-01 12:46:22 -0400

    Text Rendering Still Not Working

--- src/main/java/visuals/rendering/text/TextRenderer.java
+++ src/main/java/visuals/rendering/text/TextRenderer.java
@@ -43,7 +43,6 @@ public class TextRenderer {
 	 * The {@link VertexArrayObject} to use when rendering text. It is a rectangle.
 	 */
 	private final VertexArrayObject vao;
-	private final FrameBufferObject fbo;
 
 	private final GLContext glContext;
 
@@ -55,14 +54,12 @@ public class TextRenderer {
 	 *
 	 * @param shaderProgram the <code>TextShaderProgram</code>
 	 * @param vao           the <code>RectangleVertexArrayObject</code>
-	 * @param fbo           the <code>FrameBufferObject</code>
 	 */
-	public TextRenderer(GLContext glContext, TextureRenderer textureRenderer, ShaderProgram shaderProgram, VertexArrayObject vao, FrameBufferObject fbo) {
+	public TextRenderer(GLContext glContext, TextureRenderer textureRenderer, ShaderProgram shaderProgram, VertexArrayObject vao) {
 		this.glContext = glContext;
 		this.textureRenderer = textureRenderer;
 		this.shaderProgram = shaderProgram;
 		this.vao = vao;
-		this.fbo = fbo;
 	}
 
 	public TextRenderer(GLContext glContext) {
@@ -72,8 +69,6 @@ public class TextRenderer {
 		VertexShader vertex = TexturedTransformationVertexShader.instance();
 		FragmentShader fragment = TextFragmentShader.instance();
 		this.shaderProgram = new ShaderProgram().attach(vertex, fragment).load();
-		Texture texture = new Texture().dimensions(512, 512).load();
-		this.fbo = new FrameBufferObject().texture(texture).load();
 	}
 
 	/**
@@ -103,8 +98,11 @@ public class TextRenderer {
 	 * @param colour    the {@link Colour} (int)
 	 * @return the number of lines of text rendered
 	 */
-	public int render(Matrix4f transform, String text, float lineWidth, GameFont font, float fontSize, int colour) {
+	private int render(Matrix4f transform, String text, float lineWidth, GameFont font, float fontSize, int colour) {
 		shaderProgram.use(glContext);
+		shaderProgram.set("transform", transform);
+		shaderProgram.set("textureSampler", 0);
+		shaderProgram.set("fill", toRangedVector(colour));
 //		ShaderUniformInputList list =
 //				shaderProgram.uniforms()
 //						.set("textureSampler", 0)
@@ -113,11 +111,18 @@ public class TextRenderer {
 //						.set("fill", toRangedVector(colour));
 //				.complete();
 
+		float totalXOffset = 0;
+		float totalYOffset = 0;
 		for (int i = 0, m = text.length(); i < m; i++) {
 			char c = text.charAt(i);
 			CharacterData data = font.getCharacterDatas()[c];
 			shaderProgram.set("atlas[" + i + "]", new Vector4f(data.x(), data.y(), data.width(), data.height()));
-			shaderProgram.set("offset[" + i + "]", new Vector2f(data.xOffset(), data.yOffset()));
+			shaderProgram.set("offset[" + i + "]", new Vector2f(totalXOffset + data.xOffset(), totalYOffset + data.yOffset()));
+			totalXOffset += data.xAdvance();
+			if (totalXOffset > lineWidth) {
+				totalXOffset = 0;
+				totalYOffset += fontSize;
+			}
 		}
 		vao.drawInstanced(glContext, text.length());
 
@@ -162,80 +167,79 @@ public class TextRenderer {
 		return 0;
 	}
 
-	private void renderOneLine(float xOffset, float yOffset, String text, GameFont font, float fontSize) {
-		char[] chars = text.toCharArray();
-		Matrix4f transform = new Matrix4f().translate(-1, -1).scale(2, 2).scale(1 / glContext.width(), 1 / glContext.height());
-		float sizeMultiplier = fontSize / font.getFontSize();
-		fbo.bind(glContext);
-
-		CharacterData[] characterDataArray = font.getCharacterDatas();
-		for (char ch : chars) {
-			CharacterData c = characterDataArray[ch];
-			int xAdvance = c.xAdvance();
-			if (ch == ' ' || ch == '\t') {
-				xOffset += xAdvance * sizeMultiplier;
-				continue;
-			}
-			Matrix4f copy = transform.copy()
-					.translate(xOffset + c.xOffset() * sizeMultiplier, yOffset + c.yOffset() * sizeMultiplier)
-					.scale(c.width() * sizeMultiplier, c.height() * sizeMultiplier);
-			shaderProgram.uniforms()
-					.set("matrix4f", copy)
-					.set("width", c.width())
-					.set("height", c.height())
-					.set("x", c.x())
-					.set("y", c.y())
-					.complete();
-			vao.draw(glContext);
-			xOffset += xAdvance * sizeMultiplier;
-		}
-	}
-
-	public int calculateNumLines(String text, GameFont font, float fontSize, float lineWidth) {
-		return convertToStringPairs(text, font, fontSize, lineWidth).size();
-	}
-
-	private List<StringRenderWidth> convertToStringPairs(String text, GameFont font, float fontSize, float lineWidth) {
-		String[] splitByNewLines = text.split("\\n");
-		List<StringRenderWidth> stringPairs = new ArrayList<>();
-		for (String line : splitByNewLines) {
-			stringPairs.addAll(convertParagraphToStringPairs(line, font, fontSize, lineWidth));
-		}
-		return stringPairs;
-	}
-
-	private List<StringRenderWidth> convertParagraphToStringPairs(String text, GameFont font, float fontSize, float lineWidth) {
-		CharacterData[] characterDatas = font.getCharacterDatas();
-
-		List<StringRenderWidth> pairs = new ArrayList<>();
-
-		String[] words = text.split(" ");
-		float sizeMultiplier = fontSize / font.getFontSize();
-
-		float currentStringWidth = 0;
-		StringBuilder currentString = new StringBuilder();
-		for (String word : words) {
-			float wordWidth = 0;
-			for (int j = 0; j < word.length(); j++) {
-				wordWidth += characterDatas[word.charAt(j)].xAdvance() * sizeMultiplier;
-			}
-
-			if (currentStringWidth + wordWidth <= lineWidth) {
-				if (currentString.length() > 0) {
-					currentString.append(" ");
-					currentStringWidth += characterDatas[' '].xAdvance() * sizeMultiplier;
-				}
-				currentStringWidth += wordWidth;
-				currentString.append(word);
-			} else {
-				pairs.add(new StringRenderWidth(currentString.toString(), currentStringWidth));
-				currentStringWidth = wordWidth;
-				currentString = new StringBuilder(word);
-			}
-		}
-		pairs.add(new StringRenderWidth(currentString.toString(), currentStringWidth));
-		return pairs;
-	}
+//	private void renderOneLine(float xOffset, float yOffset, String text, GameFont font, float fontSize) {
+//		char[] chars = text.toCharArray();
+//		Matrix4f transform = new Matrix4f().translate(-1, -1).scale(2, 2).scale(1 / glContext.width(), 1 / glContext.height());
+//		float sizeMultiplier = fontSize / font.getFontSize();
+//
+//		CharacterData[] characterDataArray = font.getCharacterDatas();
+//		for (char ch : chars) {
+//			CharacterData c = characterDataArray[ch];
+//			int xAdvance = c.xAdvance();
+//			if (ch == ' ' || ch == '\t') {
+//				xOffset += xAdvance * sizeMultiplier;
+//				continue;
+//			}
+//			Matrix4f copy = transform.copy()
+//					.translate(xOffset + c.xOffset() * sizeMultiplier, yOffset + c.yOffset() * sizeMultiplier)
+//					.scale(c.width() * sizeMultiplier, c.height() * sizeMultiplier);
+//			shaderProgram.uniforms()
+//					.set("matrix4f", copy)
+//					.set("width", c.width())
+//					.set("height", c.height())
+//					.set("x", c.x())
+//					.set("y", c.y())
+//					.complete();
+//			vao.draw(glContext);
+//			xOffset += xAdvance * sizeMultiplier;
+//		}
+//	}
+//
+//	public int calculateNumLines(String text, GameFont font, float fontSize, float lineWidth) {
+//		return convertToStringPairs(text, font, fontSize, lineWidth).size();
+//	}
+//
+//	private List<StringRenderWidth> convertToStringPairs(String text, GameFont font, float fontSize, float lineWidth) {
+//		String[] splitByNewLines = text.split("\\n");
+//		List<StringRenderWidth> stringPairs = new ArrayList<>();
+//		for (String line : splitByNewLines) {
+//			stringPairs.addAll(convertParagraphToStringPairs(line, font, fontSize, lineWidth));
+//		}
+//		return stringPairs;
+//	}
+//
+//	private List<StringRenderWidth> convertParagraphToStringPairs(String text, GameFont font, float fontSize, float lineWidth) {
+//		CharacterData[] characterDatas = font.getCharacterDatas();
+//
+//		List<StringRenderWidth> pairs = new ArrayList<>();
+//
+//		String[] words = text.split(" ");
+//		float sizeMultiplier = fontSize / font.getFontSize();
+//
+//		float currentStringWidth = 0;
+//		StringBuilder currentString = new StringBuilder();
+//		for (String word : words) {
+//			float wordWidth = 0;
+//			for (int j = 0; j < word.length(); j++) {
+//				wordWidth += characterDatas[word.charAt(j)].xAdvance() * sizeMultiplier;
+//			}
+//
+//			if (currentStringWidth + wordWidth <= lineWidth) {
+//				if (currentString.length() > 0) {
+//					currentString.append(" ");
+//					currentStringWidth += characterDatas[' '].xAdvance() * sizeMultiplier;
+//				}
+//				currentStringWidth += wordWidth;
+//				currentString.append(word);
+//			} else {
+//				pairs.add(new StringRenderWidth(currentString.toString(), currentStringWidth));
+//				currentStringWidth = wordWidth;
+//				currentString = new StringBuilder(word);
+//			}
+//		}
+//		pairs.add(new StringRenderWidth(currentString.toString(), currentStringWidth));
+//		return pairs;
+//	}
 
 	public void alignLeft() {
 		hAlign = ALIGN_LEFT;
